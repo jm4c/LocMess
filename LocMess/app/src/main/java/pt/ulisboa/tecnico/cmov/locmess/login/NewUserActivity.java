fb74ada2b218b7eafdf346e9eb173995c74e1023
jm4c <joao.d.j.machado@gmail.com>
2017-04-20 17:48:48 +0100

    Fixed bug with switched layouts in 4e1459e89a4bfb22b7945229846b47fb9842b233

--- LocMess/app/src/main/java/pt/ulisboa/tecnico/cmov/locmess/login/NewUserActivity.java
+++ LocMess/app/src/main/java/pt/ulisboa/tecnico/cmov/locmess/login/NewUserActivity.java
@@ -10,6 +10,6 @@ public class NewUserActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_new_user);
     }
 }
