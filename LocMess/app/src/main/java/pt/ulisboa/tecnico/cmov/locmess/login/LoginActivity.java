d01564d3a41178dcd4743655610cea2fe740d433
beatrizagf <beatrizabecasis@gmail.com>
2017-04-22 03:18:55 +0100

    Acrescentei as actividades do login e do newUser. Para fazer login é só necessário colocar no name pelo menos 3 caracteres e na password entre 4 e 10 caracteres O método login e signup são temporários, depois com o servidor é preciso mudar

--- LocMess/app/src/main/java/pt/ulisboa/tecnico/cmov/locmess/login/LoginActivity.java
+++ LocMess/app/src/main/java/pt/ulisboa/tecnico/cmov/locmess/login/LoginActivity.java
@@ -1,15 +1,115 @@
 package pt.ulisboa.tecnico.cmov.locmess.login;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import pt.ulisboa.tecnico.cmov.locmess.R;
+import pt.ulisboa.tecnico.cmov.locmess.inbox.InboxActivity;
+
 
 public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "LoginActivity";
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button loginButton = (Button) findViewById(R.id.btn_login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                login();
+            }
+        });
+
+        TextView signupLink = (TextView) findViewById(R.id.link_signup);
+        signupLink.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Start the Signup activity
+                Intent intent = new Intent(getApplicationContext(), NewUserActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void login() {
+        Log.d(TAG, "Login");
+
+        if (!validate()) {
+            onLoginFailed();
+            return;
+        }
+
+        Button loginButton = (Button) findViewById(R.id.btn_login);
+        loginButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Authenticating...");
+        progressDialog.show();
+
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onLoginSuccess or onLoginFailed
+                        onLoginSuccess();
+                        // onLoginFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 3000);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        // disable going back to the MainActivity
+        moveTaskToBack(true);
+    }
+
+    public void onLoginSuccess() {
+        Button loginButton = (Button) findViewById(R.id.btn_login);
+        loginButton.setEnabled(true);
+        Intent i = new Intent(getApplicationContext(), InboxActivity.class);
+        startActivity(i);
+        //finish();
+    }
+
+    public void onLoginFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+        Button loginButton = (Button) findViewById(R.id.btn_login);
+        loginButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+        EditText passwordText = (EditText) findViewById(R.id.input_password);
+
+        String password = passwordText.getText().toString();
+
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            passwordText.setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        } else {
+            passwordText.setError(null);
+        }
+
+        return valid;
     }
 }
