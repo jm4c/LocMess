27fd4e2e0607101bd548fec942165970ec35100e
jm4c <joao.d.j.machado@gmail.com>
2017-04-27 03:16:27 +0100

    Working MainMenu. Close #7 Still some tweaks left to use it in other activities.

--- LocMess/app/src/main/java/pt/ulisboa/tecnico/cmov/locmess/MainMenuActivity.java
+++ LocMess/app/src/main/java/pt/ulisboa/tecnico/cmov/locmess/MainMenuActivity.java
@@ -1,26 +1,54 @@
 package pt.ulisboa.tecnico.cmov.locmess;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Switch;
 import android.widget.Toast;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import pt.ulisboa.tecnico.cmov.locmess.inbox.InboxActivity;
 import pt.ulisboa.tecnico.cmov.locmess.location.LocationActivity;
 import pt.ulisboa.tecnico.cmov.locmess.login.LoginActivity;
+import pt.ulisboa.tecnico.cmov.locmess.model.Location;
 import pt.ulisboa.tecnico.cmov.locmess.outbox.OutboxActivity;
+import pt.ulisboa.tecnico.cmov.locmess.outbox.PostMessageActivity;
 import pt.ulisboa.tecnico.cmov.locmess.profile.ProfileActivity;
 
+import static pt.ulisboa.tecnico.cmov.locmess.model.TestData.getLocations;
+
 public class MainMenuActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        /*implements NavigationView.OnNavigationItemSelectedListener*/ {
     private static final String activityTitle = "LocMess - Main Menu";
+
+    private static final int MENU_INBOX = 0;
+    private static final int MENU_OUTBOX = 1;
+    private static final int MENU_PROFILE = 2;
+    private static final int MENU_LOCATIONS = 3;
+    private static final int MENU_LOGOUT = 4;
+
+
+    private static final String[] menu = {
+            "Inbox",
+            "Outbox",
+            "Locations",
+            "Profile",
+            "Logout"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,62 +57,59 @@ public class MainMenuActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(activityTitle);
         setSupportActionBar(toolbar);
-
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
     }
 
 
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        Intent i;
-
-        switch(item.getItemId()){
-            case R.id.nav_inbox:
-                i = new Intent(this, InboxActivity.class);
-                break;
-            case R.id.nav_outbox:
-                i = new Intent(this, OutboxActivity.class);
-                break;
-            case R.id.nav_locations:
-                i = new Intent(this, LocationActivity.class);
-                Toast.makeText(this, "TODO", Toast.LENGTH_LONG);
-                break;
-            case R.id.nav_profile:
-                i = new Intent(this, ProfileActivity.class);
-                break;
-            case R.id.nav_logout:
-                i = new Intent(this, LoginActivity.class);
-                break;
-            default:
-                i = null;
-                return false;
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-
-        startActivity(i);
-
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbar_buttons, menu);
         return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final List<String> menu = getMenu();
+
+        CharSequence[] cs = menu.toArray(new CharSequence[menu.size()]);
+        builder.setTitle("Main Menu")
+                .setItems(cs, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        changeActivity(which);
+                    }
+                });
+        builder.create().show();
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void changeActivity(int which) {
+        Intent i;
+        switch (which){
+            case MENU_INBOX:
+                i = new Intent(MainMenuActivity.this, InboxActivity.class);
+                break;
+            case MENU_OUTBOX:
+                i = new Intent(MainMenuActivity.this, OutboxActivity.class);
+                break;
+            case MENU_LOCATIONS:
+                i = new Intent(MainMenuActivity.this, LocationActivity.class);
+                break;
+            case MENU_PROFILE:
+                i = new Intent(MainMenuActivity.this, ProfileActivity.class);
+                break;
+            default: //LOGOUT
+                i = new Intent(MainMenuActivity.this, LoginActivity.class);
+                //TODO remove credentials/login token
+                break;
+        }
+        startActivity(i);
+    }
+
+    public List<String> getMenu(){
+        return Arrays.asList(menu);
+    }
+
+
 }
